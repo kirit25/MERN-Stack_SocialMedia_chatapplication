ebdc01814afa4c5e7ca564e7a95edfd8ba7327f9
kirit25 <kirit25parmar@gmail.com>
2021-07-22 19:05:41 +0530

    Initial commit

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -11,7 +11,7 @@ export default function Topbar() {
     <div className="topbarContainer">
       <div className="topbarLeft">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">FunSocial</span>
+          <span className="logo">SpiritualSocial</span>
         </Link>
       </div>
       <div className="topbarCenter">
@@ -25,8 +25,8 @@ export default function Topbar() {
       </div>
       <div className="topbarRight">
         <div className="topbarLinks">
-          <span className="topbarLink">Homepage</span>
-          <span className="topbarLink">Timeline</span>
+          {/* <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span> */}
         </div>
         <div className="topbarIcons">
           <div className="topbarIconItem">
